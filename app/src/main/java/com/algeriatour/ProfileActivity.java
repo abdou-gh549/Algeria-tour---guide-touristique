b6b3ba66c12c88e488bcd5d8f2a02137c46fa78b
abdou-gh549 <abdellah.abdou21@gmail.com>
2018-05-08 15:38:03 +0100

    profile layout

---
+++ app/src/main/java/com/algeriatour/ProfileActivity.java
@@ -0,0 +1,16 @@
+package com.algeriatour;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.rengwuxian.materialedittext.MaterialEditText;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+    }
+}
